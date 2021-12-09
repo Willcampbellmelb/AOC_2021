c0372e5729c5ba75dffe4862f2dae124b02be5ab
William Campbell <wacampbel@gmail.com>
2021-12-08 18:37:00 -0800

    Part 1 correct A

---
+++ Day_03/src/turtle_world/TurtleWorld.cpp
@@ -0,0 +1,12 @@
+#include "Turtle.h"
+#include "tools.h"
+using std::cout;
+using std::endl;
+int main() {
+
+  cout << PowerConsumption("../data/input.txt", 1000) << endl;
+
+  cout OxGenRating()
+
+      return 0;
+}
